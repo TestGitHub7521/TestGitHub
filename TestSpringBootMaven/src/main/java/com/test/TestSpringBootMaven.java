b56867ddf9bf82df598287525a8ee49b53f14d34
3650203 <3650203@BRK-L6274TNQ.corp.ds.fedex.com>
2018-06-11 20:40:12 -0400

    first commit

---
+++ TestSpringBootMaven/src/main/java/com/test/TestSpringBootMaven.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootMaven {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootMaven.class, args);
+	}
+}
---
+++ TestSpringBootMaven/src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/hello")
+	public String sayHello()
+	{
+		return "green";
+	}
+
+}
